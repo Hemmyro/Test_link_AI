7dec745a3c4a92d7f9aecb5ee833c2f5bb5f6c4f
Hemmyro <102832002+Hemmyro@users.noreply.github.com>
2023-03-25 03:22:36 +0300

    Update discord_bot_II.py

--- discord_bot_II.py
+++ discord_bot_II.py
@@ -1,19 +1,1 @@
-import discord
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('Logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!midjourney'):
-        # запрос к ИИ Midjourney
-        response = "Ответ от ИИ Midjourney"
-        await message.channel.send(response)
-
-client.run('MTA4ODk2OTcwODgxODE0OTQyNg.Gcu-tI.3MQ3PHZKWIv_480oBc8Aqky17noTRfFo46VT_g')
